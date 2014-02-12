7e4c4cb5563665f1d6c66e81e1e861878fa5c3dd
Lavi Avigdor 2 <laviavigdor@dvir-laptop.europe.greenfieldnet.com>
2014-02-12 14:39:07 +0200

    Initial Commit

---
+++ menuapp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  menuapp
+//
+//  Created by Lavi Avigdor 2 on 2/12/14.
+//  Copyright (c) 2014 Toluna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ menuapp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  menuapp
+//
+//  Created by Lavi Avigdor 2 on 2/12/14.
+//  Copyright (c) 2014 Toluna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
